d290887090ec223950b9fc8078b2cf333e294934
Jones-ez <infolucywynnejones@gmail.com>
2026-08-01 21:53:30 +0100

    Update makeAyahJson.py
    update file

--- makeAyahJson.py
+++ makeAyahJson.py
@@ -1,1 +1,40 @@
-\\wsl.localhost\Ubuntu\home\luciawyn\tafsir_ruhul_maani\makeAyahsJson.py
+import json
+
+def ayahObject(juz, surah, ayah, arabic):
+    return {
+        'juz': juz,
+        'surah': surah,
+        'ayah': ayah,
+        'arabic': arabic
+    }
+
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
+
+for surah in range(1, 115):
+    with open(f"data/surah/surah_"+str(surah)+".json") as surahData:
+        surahData = json.load(surahData)
+        ayahNumber = surahData['count']
+        juzs = surahData['juz']
+        ayahs = []
+
+        for juz in juzs:
+            start = int(juz['verse']['start'].replace('verse_', ''))
+            end = int(juz['verse']['end'].replace('verse_', ''))
+            for ayah in range(1, (ayahNumber+1)):
+                if((ayah>=start)&(ayah<=end)):
+                    ayahs.append(
+                        ayahObject(
+                            int(juz['index']),
+                            surah,
+                            ayah,
+                            (surahData['verse']['verse_'+str(ayah)])
+                        )
+                    )       
+
+        with open(f'data/ayahs/arabicBySurahWithJuzData/surah_'+str(surah)+'.json', 'w', encoding="utf-8") as surahFile:
+            json.dump(ayahs, surahFile, default=set_default, ensure_ascii=False, indent=3)
+            print('surah_'+str(surah)+'.json saved')
+            
